efc35a1246b0af12486fbf97f3df076aaa571e57
Anil Dutt <anil.devdutt@gmail.com>
2018-06-19 12:24:19 +0000

    first commit

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="js/timer.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/p5.js"></script>
         <script src="js/sketch.js"></script>
