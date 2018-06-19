fd6a3a098ee70c909bc56d8c09eb58668a8158ee
Anil Dutt <anil.devdutt@gmail.com>
2018-06-19 14:02:32 +0000

    full page graphics

--- js/sketch.js
+++ js/sketch.js
@@ -4,14 +4,15 @@ let f = [];
 let ang  = 0;
 const imgSize = 40;
 let a = 0;
+const flowersCount = 40;
 
 function preload() {
     flower = loadImage("img/flower.svg");
 }
 
 function initFlower() {
-    for(let i = 0; i < 30; i++) {
-       let tmp = [random(windowWidth), -random(600) - imgSize, random(360), 1, random(10)];
+    for(let i = 0; i < flowersCount; i++) {
+       let tmp = [random(windowWidth), -random(windowHeight) - imgSize, random(360), 1, random(10)];
        f.push(tmp);
     }
 }
@@ -21,16 +22,16 @@ function updateFlower() {
     a += 1;
     if(a >= 360)
         a = 0;
-    for(let i = 0; i < 30; i++) {
+    for(let i = 0; i < flowersCount; i++) {
         push();
-        if(f[i][1] > 600 + imgSize) {
+        if(f[i][1] > windowHeight + imgSize) {
             f[i][0] = random(windowWidth);
-            f[i][1] = -random(600) - imgSize;
+            f[i][1] = -random(windowHeight) - imgSize;
             f[i][3] = 1;
         }
         f[i][0] += offset;
         f[i][1] += f[i][3];
-        f[i][3] += 0.001 * f[i][4];
+        f[i][3] += 0.002 * f[i][4];
         translate(f[i][0], f[i][1]);
         rotate(f[i][2]);
         if(f[i][2] < 360) {
@@ -43,11 +44,15 @@ function updateFlower() {
     }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
     initFlower();
     angleMode(DEGREES);
     imageMode(CENTER);
-    let cnv = createCanvas(windowWidth,600);
+    let cnv = createCanvas(windowWidth,windowHeight);
     cnv.class("p5canvas");
     textSize(36);
     textAlign(CENTER);
@@ -62,12 +67,12 @@ function draw() {
 
     let etaMilliseconds = weddingDay - Date.now();
     let etaDays = Math.floor(etaMilliseconds / 86400000);
-    let etaHours = Math.floor((etaMilliseconds % 86400000) / 3600000);
-    let etaMinutes = Math.floor((etaMilliseconds % 3600000) / 60000);
-    let etaSeconds = Math.floor((etaMilliseconds % 60000) / 1000);
+    let etaHours = Math.abs(Math.floor((etaMilliseconds % 86400000) / 3600000));
+    let etaMinutes = Math.abs(Math.floor((etaMilliseconds % 3600000) / 60000));
+    let etaSeconds = Math.abs(Math.floor((etaMilliseconds % 60000) / 1000));
     
-    let eta = ((etaDays > 9) ? etaDays : "0" + etaDays) + ":" + ((etaHours > 9) ? etaHours : "0" + etaHours) + ":" +  ((etaMinutes > 9) ? etaMinutes : "0" + etaMinutes) + ":" + ((etaSeconds > 9) ? etaSeconds : "0" + etaSeconds);
-    text(weddingDay.toString(), windowWidth/2, 150);
+    let eta = ((etaDays > 9 || etaDays < 0) ? etaDays : "0" + etaDays) + ":" + ((etaHours > 9) ? etaHours : "0" + etaHours) + ":" +  ((etaMinutes > 9) ? etaMinutes : "0" + etaMinutes) + ":" + ((etaSeconds > 9) ? etaSeconds : "0" + etaSeconds);
+    //text(weddingDay.toString(), windowWidth/2, 150);
     text("Log Horizon: A point of no return", windowWidth/2, 250);
     text("ETA: " + eta, windowWidth/2, 350);
     
