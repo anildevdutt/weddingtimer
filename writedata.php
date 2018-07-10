550d5d392f076fa1617f4e75a01cd0882592f930
Anil Dutt <anil.devdutt@gmail.com>
2018-07-10 09:46:22 +0000

    updated

--- writedata.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $myfile = fopen("messages.json", "ab") or die("Unable to open file!");
-    $msg->uname = $_POST["uname"];
-    $msg->message = $_POST["msg"];
-    $myJSON = json_encode($msg)."\n";
-    fwrite($myfile, $myJSON);
-    header("Refresh:0; url=index.php");
-?>
